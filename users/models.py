1fa40d8ed1c0914e532fe4ed27a4ba6840ce24b2
General202 <user.den.ua@gmail.com>
2025-05-06 20:43:42 +0300

    add table

--- users/models.py
+++ users/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    role = models.CharField(max_length=50, choices=[('admin', 'Адміністратор'), ('user', 'Користувач')], default='user')
+
